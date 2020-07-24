138a04ee996740fe75808a087ca1ae9cbf804f5f
TBS093A <zukkamil.44@gmail.com>
2020-07-24 10:53:39 +0200

    add django_extensions generate UML

---
+++ migrate.sh
@@ -0,0 +1,3 @@
+python manage.py makemigrations
+python manage.py migrate
+python manage.py graph_models -a -g -o class_diagram.png
--- packages.sh
+++ packages.sh
@@ -1,5 +1,12 @@
 pip install django
 pip install django-cors-headers
+
 pip install channels_redis
 pip install requests
 pip install pyjwt
+
+pip install django_extensions
+pip install pydotplus
+
+# for runing generate UML:
+# apt-get install graphviz
