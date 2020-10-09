0d004889a85c4b4135fca5baff439b1081ddca17
TBS093A <zukkamil.44@gmail.com>
2020-10-09 15:03:17 +0200

    fix migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
-python manage.py graph_models -a -g -o class_diagram_by_apps.png
-python manage.py graph_models -a -o class_diagram.png
+# python manage.py graph_models -a -g -o class_diagram_by_apps.png
+# python manage.py graph_models -a -o class_diagram.png
